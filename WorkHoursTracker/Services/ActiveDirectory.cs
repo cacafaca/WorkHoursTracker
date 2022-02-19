0e1f11a40bcdc6614a78cf5062bcef043c621672
Nemanja Simović <nemanja.simovic@gmail.com>
2022-02-19 02:56:40 +0100

    Fix for non-domain computers.

--- WorkHoursTracker/Services/ActiveDirectory.cs
+++ WorkHoursTracker/Services/ActiveDirectory.cs
@@ -1,20 +1,46 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using System.DirectoryServices.AccountManagement;
-using System;
+﻿using System;
 using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 
 namespace ProCode.WorkHoursTracker.Services
 {
     public class ActiveDirectory
     {
+        #region Fields
+        const string _notApplicable = "N/A";
         private string _firstAndLastName;
         private string _title;
         private string _department;
+        #endregion
 
+        #region Constructors
         public ActiveDirectory()
         {
             Read();
+        }
+        #endregion
+
+        #region Properties
+        public string FirstAndLastName { get { return _firstAndLastName; } }
+        public string Title { get { return _title; } }
+        public string Department { get { return _department; } }
+        #endregion
+
+        #region Methods
+        public void Read()
+        {
+            try
+            {
+                PrincipalContext domain = new PrincipalContext(ContextType.Domain);
+                UserPrincipal userPrincipal = UserPrincipal.FindByIdentity(domain, Environment.UserName);
+                _firstAndLastName = GetProperty(userPrincipal, "cn");
+                _title = GetProperty(userPrincipal, "title");
+                _department = GetProperty(userPrincipal, "department");
+            }
+            catch (PrincipalServerDownException)
+            {
+                SetNonDomainDefaultValues();
+            }
         }
 
         private string GetProperty(Principal principal, string property)
@@ -26,19 +52,12 @@ namespace ProCode.WorkHoursTracker.Services
                 return string.Empty;
         }
 
-        public void Read()
-        {
-            PrincipalContext domain = new PrincipalContext(ContextType.Domain);
-            UserPrincipal userPrincipal = UserPrincipal.FindByIdentity(domain, Environment.UserName);
-            _firstAndLastName = GetProperty(userPrincipal, "cn");
-            _title = GetProperty(userPrincipal, "title");
-            _department = GetProperty(userPrincipal, "department");
+        void SetNonDomainDefaultValues()
+        {            
+            _firstAndLastName = Environment.UserName;
+            _title = _notApplicable;
+            _department = _notApplicable;
         }
-
-        public string FirstAndLastName { get { return _firstAndLastName; } }
-
-        public string Title { get { return _title; } }
-
-        public string Department { get { return _department; } }
+        #endregion
     }
 }
